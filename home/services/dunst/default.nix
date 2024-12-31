0e324762615fb011b64e5b1d0f0b5352d27cfa13
Jake Ginesin <jakeginesin@gmail.com>
2024-12-31 07:02:46 -0500

    139 current  2024-12-31 07:02:39  25.05.20241217.d3c42f1     6.6.66                          *

--- home/services/dunst/default.nix
+++ home/services/dunst/default.nix
@@ -5,6 +5,19 @@
 }: {
   services.dunst = {
     enable = true;
-    configFile = ./dunstrc;
+    # configFile = ./dunstrc;
+    settings = {
+      global = {
+        monitor = 0;
+        follow = "mouse";
+        width = 300;
+        height = 300;
+        origin = "top-right";
+        offset = "10x50";
+        scale = 0;
+        notification_limit = 0;
+        progress_bar = true;
+      };
+    };
   };
 }
