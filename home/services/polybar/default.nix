69d19ee49acd682d3fc2ca8f3a18caa18c3f3e80
Jake Ginesin <jakeginesin@gmail.com>
2025-01-02 01:35:50 -0500

    161 current  2025-01-02 01:35:42  25.05.20241217.d3c42f1     6.6.66                          *

--- home/services/polybar/default.nix
+++ home/services/polybar/default.nix
@@ -1,13 +1,18 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   services.polybar = {
     enable = true;
     extraConfig = builtins.readFile ./config.ini;
     script = ''
-      polybar -q -r -c /home/synchronous/.config/polybar/config.ini mybar & disown
+      polybar -q -r -c /home/synchronous/.config/polybar/config.ini mybar > /tmp/polybar & disown
     '';
   };
+
+  home.activation.polybarStartup = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    polybar -q -r -c /home/synchronous/.config/polybar/config.ini mybar > /tmp/polybar & disown
+  '';
 }
