57db910cd822eb1bac237bfd44f37ae3efbc3e49
Jake Ginesin <jakeginesin@gmail.com>
2025-01-02 23:24:32 -0500

    166 current  2025-01-02 02:05:41  25.05.20241217.d3c42f1     6.6.66                          *

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
               # inputs.emacs-overlay.overlay -- breaks doom on 30.??
             ];
 
-            home.packages = with pkgs; [xrandr procps bspwm sxhkd];
+            home.packages = with pkgs; [xrandr procps polybar bspwm sxhkd];
           };
           specialArgs = {inherit inputs;};
           # extraSpecialArgs = {inherit inputs;};
