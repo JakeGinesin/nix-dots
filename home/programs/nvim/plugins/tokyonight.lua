cadf285ec41c1d9c5af385463b2588b665ba51cd
Jake Ginesin <jakeginesin@gmail.com>
2025-01-10 23:16:32 -0500

    197 current  2025-01-10 23:16:23  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/plugins/tokyonight.lua
+++ home/programs/nvim/plugins/tokyonight.lua
@@ -9,8 +9,8 @@ require("tokyonight").setup({
   },
   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
   on_colors = function(colors)
-    colors.hint = colors.orange
-    colors.bg = "#ff0000"
+    -- colors.hint = colors.orange
+    colors.bg = "#0d0d0d"
     colors.fg = "#e3e1e1"
   end
 })
