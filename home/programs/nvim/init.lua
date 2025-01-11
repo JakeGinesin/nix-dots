5bd9479269f57d2ee6df3c6c02fd43820efcc6f6
Jake Ginesin <jakeginesin@gmail.com>
2025-01-10 23:09:20 -0500

    194 current  2025-01-10 23:09:10  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/init.lua
+++ home/programs/nvim/init.lua
@@ -143,3 +143,19 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "setlocal wrap"
 })
 
+
+
+require("tokyonight").setup({
+  -- use the night style
+  style = "night",
+  -- disable italic for functions
+  styles = {
+    functions = {}
+  },
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  on_colors = function(colors)
+    colors.hint = colors.orange
+    colors.bg = "#ff0000"
+    colors.fg = "#e3e1e1"
+  end
+})
