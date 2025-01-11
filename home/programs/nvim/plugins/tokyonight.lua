ec4a544039640990ba87848a512ddebf7301a3d6
Jake Ginesin <jakeginesin@gmail.com>
2025-01-10 23:03:04 -0500

    191 current  2025-01-10 23:02:55  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/plugins/tokyonight.lua
+++ home/programs/nvim/plugins/tokyonight.lua
@@ -1,6 +1,6 @@
 -- Tokyonight configuration in Lua
 
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "day"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
