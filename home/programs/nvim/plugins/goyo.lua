2d506487b9314b0415b47b498c8a4100b98c994f
Jake Ginesin <jakeginesin@gmail.com>
2025-01-10 22:36:09 -0500

    189 current  2025-01-10 22:36:00  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/plugins/goyo.lua
+++ home/programs/nvim/plugins/goyo.lua
@@ -1,3 +1,5 @@
+local opts = { noremap = true, silent = true }
+
 -- "Focus" commands that rely on the Goyo plugin
 vim.api.nvim_set_keymap("n", "<C-a>z", ":Goyo 80<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-a>q", ":Goyo!<CR>", opts)
