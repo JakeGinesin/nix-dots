7a518a6bfa044d2ca733099aa526f1febacb9fc2
Jake Ginesin <jakeginesin@gmail.com>
2025-01-18 12:59:56 -0500

    270 current  2025-01-18 12:59:44  25.05.20241217.d3c42f1     6.6.66                          *

--- home/fonts/default.nix
+++ home/fonts/default.nix
@@ -4,6 +4,8 @@
     fontconfig
   ];
 
+  # god's chosen fontset
   home.file.".local/share/fonts/NotoSans-Regular.ttf".source = ./NotoSans-Regular.ttf;
   home.file.".local/share/fonts/NotoSansSymbols-Regular.ttf".source = ./NotoSansSymbols-Regular.ttf;
+  home.file.".local/share/fonts/JetBrains-Mono-Nerd-Font-Complete-Regular.ttf".source = ./JetBrains-Mono-Nerd-Font-Complete-Regular.ttf;
 }
