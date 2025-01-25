77bebaca4a17b4323a72bc4f14589282ac19f4c2
Jake Ginesin <jakeginesin@gmail.com>
2025-01-25 01:38:29 -0500

    290 current  2025-01-25 01:30:21  25.05.20241217.d3c42f1     6.6.66                          *

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
