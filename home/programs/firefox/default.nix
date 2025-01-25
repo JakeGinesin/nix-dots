efece8b6e35daa496ec7c9901e6d21fd7d84f5c4
Jake Ginesin <jakeginesin@gmail.com>
2025-01-25 02:44:35 -0500

    297 current  2025-01-25 02:44:21  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -211,6 +211,11 @@
           installation_mode = "force_installed";
           default_area = "menupanel";
         };
+        "zotero@chnm.gmu.edu" = {
+          install_url = "https://www.zotero.org/download/connector/dl?browser=firefox&version=5.0.151";
+          installation_mode = "force_installed";
+          default_area = "navbar";
+        };
       };
     };
     profiles.synchronous = {
