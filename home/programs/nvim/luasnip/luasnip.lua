b623cad0f0d13f31035f46f599d2557e5698afa1
Jake Ginesin <jakeginesin@gmail.com>
2025-02-15 01:29:38 -0500

    326 current  2025-02-15 01:29:18  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/luasnip/luasnip.lua
+++ home/programs/nvim/luasnip/luasnip.lua
@@ -1,7 +1,3 @@
-require("luasnip.loaders.from_lua").load({paths = "./snippets/"})
-
-require("luasnip.loaders.from_lua").lazy_load({paths = "./snippets/"})
-
 vim.cmd[[
 " Use Tab to expand and jump through snippets
 imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
@@ -27,15 +23,7 @@ require("luasnip").config.set_config({ -- Setting LuaSnip config
   store_selection_keys = "<Tab>",
 })
 
--- Abbreviations used in this article and the LuaSnip docs
-local ls = require("luasnip")
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep
 
+require("luasnip.loaders.from_lua").load({paths = "./snippets/"})
+
+require("luasnip.loaders.from_lua").lazy_load({paths = "./snippets/"})
