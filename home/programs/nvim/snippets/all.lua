cf5635225a15feacf15a1d81763d85f001e4b159
Jake Ginesin <jakeginesin@gmail.com>
2025-02-16 17:46:57 -0500

    340 current  2025-02-16 17:46:43  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/snippets/all.lua
+++ home/programs/nvim/snippets/all.lua
@@ -1,14 +1,13 @@
-local ls = require "luasnip"
+local ls = require("luasnip")
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
-local extras = require("luasnip.extras")
-local rep = extras.rep
-local fmt = require("luasnip.extras.fmt").fmt
-local c = ls.choice_node
 local f = ls.function_node
 local d = ls.dynamic_node
-local sn = ls.snippet_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 ls.add_snippets("lua", {
