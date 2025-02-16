373a1a7c9eee953660b0ba0a94aa3aca1e81c070
Jake Ginesin <jakeginesin@gmail.com>
2025-02-16 16:43:33 -0500

    333 current  2025-02-16 16:43:16  25.05.20241217.d3c42f1     6.6.66                          *

--- home/programs/nvim/luasnip/luasnip.lua
+++ home/programs/nvim/luasnip/luasnip.lua
@@ -25,15 +25,30 @@ require("luasnip").config.set_config({ -- Setting LuaSnip config
 
 local ls = require "luasnip"
 local s = ls.snippet
-local s = ls.text_node
+local t = ls.text_node
 local i = ls.insert_node
+local extras = require("luasnip.extras")
+local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
+local c = ls.choice_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
 
 ls.add_snippets("lua", {
-  s("hello", {
-    t('print("hello '),
-    i(1),
-    t(' world")')
-  })
+    s("hello", {
+        t('print("hello '),
+        i(1),
+        t(' world")')
+    }),
+
+    s("if", {
+        t('if '),
+        i(1, "true"),
+        t(' then '),
+        i(2),
+        t(' end')
+    })
 })
 
 
