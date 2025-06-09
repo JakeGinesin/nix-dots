6dcc2a234eae56773d62480c60f149cbc0763c71
Jake Ginesin <jakeginesin@gmail.com>
2025-06-09 02:12:59 -0400

    421 current  2025-06-09 02:12:33  25.05.20250424.f771eb4     6.12.24                          *

--- system/services/syncthing/default.nix
+++ system/services/syncthing/default.nix
@@ -6,8 +6,27 @@
   services.syncthing = {
     enable = true;
     guiAddress = "localhost:8384";
+    user = "synchronous";
+    group = "users";
+    dataDir = "/home/synchronous";
     # user = "synchronous";
     # group = "syncthing";
     openDefaultPorts = true;
+
+    settings = {
+      devices = {
+        cluster = {id = "RBT7JA4-DFQ33SG-JUWJOQ2-MLPVLV4-SL2NATS-VZBB5M5-L7CHO3N-UTBOGA4";};
+      };
+      folders = {
+        notes = {
+          id = "notes";
+          label = "Notes";
+          path = "/home/synchronous/notes";
+          type = "sendreceive";
+
+          devices = ["cluster"];
+        };
+      };
+    };
   };
 }
