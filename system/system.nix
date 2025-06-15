7ee24adc96033c0483991fb09e808f8a96562e32
Jake Ginesin <jakeginesin@gmail.com>
2025-06-16 00:22:28 +0900

    434 current  2025-06-16 00:22:08  25.05.20250424.f771eb4     6.12.24                          *

--- system/system.nix
+++ system/system.nix
@@ -14,7 +14,8 @@
   ];
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  # time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Seoul";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
