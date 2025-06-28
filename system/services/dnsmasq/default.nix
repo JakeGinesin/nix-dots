651ed14e4e8b6ab1a84c0b9d6fffe42366544990
Jake Ginesin <jakeginesin@gmail.com>
2025-06-28 02:28:43 -0400

    447 current  2025-06-28 02:28:19  25.05.20250424.f771eb4     6.12.24                          *

--- system/services/dnsmasq/default.nix
+++ system/services/dnsmasq/default.nix
@@ -7,7 +7,7 @@
     enable = true;
     resolveLocalQueries = true;
     settings = {
-      conf-dir = "var/lib/dnsmasq/conf.d";
+      conf-dir = "/var/lib/dnsmasq/conf.d";
       # listen-address = "127.0.0.1";
       # port = 53535; # a free address lol
     };
