d039a5320df06409a44c6fe5515c624f8de71182
Jake Ginesin <jakeginesin@gmail.com>
2025-07-09 20:40:22 -0400

    478 current  2025-07-09 20:39:29  25.05.20250424.f771eb4     6.12.24                          *

--- home/programs/nvim/init.lua
+++ home/programs/nvim/init.lua
@@ -164,3 +164,10 @@ vim.api.nvim_create_autocmd("FileType", {
   group = wrapLineInTexFile,
   command = "setlocal wrap"
 })
+
+-- idk why 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
