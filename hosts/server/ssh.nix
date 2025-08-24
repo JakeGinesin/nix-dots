6478b8be2df3744dd6e0ca33e59ab180f5bf2a2d
Jake Ginesin <jakeginesin@gmail.com>
2025-08-24 04:00:47 -0400

    generation 499 25.11.20250714.62e0f05

--- hosts/server/ssh.nix
+++ hosts/server/ssh.nix
@@ -10,10 +10,10 @@
   services.openssh.settings.PermitRootLogin = "no";
 
   # Add your authorized key for a specific user
-  users.users.yourusername = {
+  users.users.synchronous = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
-      config.age.secrets.ssh-pub
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEw4Uqg9UBakoOpS4nVGE3ePKHnst0+02lFN04n2IyKb ginesin.j@northeastern.edu"
     ];
   };
 }
