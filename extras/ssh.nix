8d8cd3b45fb08b03c7683a75d1dae93fdc7a3980
Jake Ginesin <jakeginesin@gmail.com>
2025-09-01 01:42:17 -0400

    generation 508 25.11.20250714.62e0f05

--- extras/ssh.nix
+++ extras/ssh.nix
@@ -12,6 +12,7 @@
   # Add your authorized key for a specific user
   users.users.synchronous = {
     isNormalUser = true;
+    shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEw4Uqg9UBakoOpS4nVGE3ePKHnst0+02lFN04n2IyKb ginesin.j@northeastern.edu"
     ];
