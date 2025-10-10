6f8aa3a84c68801bd4e6b610f336665fe80c9066
Jake Ginesin <jakeginesin@gmail.com>
2025-10-10 18:58:37 -0400

    generation 525 25.11.20250714.62e0f05

--- home/programs/nvim/plugins/smear.lua
+++ home/programs/nvim/plugins/smear.lua
@@ -1,19 +1,10 @@
 require('smear_cursor').setup({
-  -- Main options
-  stiffness = 0.4,                    -- [0, 1] - cursor follow speed
-  trailing_stiffness = 0.45,          -- [0, 1] - trail follow speed
-  distance_stop_animating = 0.1,      -- stop when close enough
-  
-  -- Insert mode
-  smear_insert_mode = true,
-  stiffness_insert_mode = 0.5,
-  trailing_stiffness_insert_mode = 0.5,
-  
-  -- Visual tweaks
-  cursor_color = "#d3cdc3",           -- match your cursor color
-  legacy_computing_symbols_support = false,
-  
-  -- Optional
-  smear_between_buffers = true,
-  smear_between_neighbor_lines = true,
+  scroll_buffer_space = false,
+  smear_between_buffers = false,
+  smear_between_neighbor_lines = false,
+  stiffness = 0.5,
+  trailing_stiffness = 0.5,
+  matrix_pixel_threshold = 0.5,
+  damping = 0.95,    
+  smear_insert_mode = false,
 })
