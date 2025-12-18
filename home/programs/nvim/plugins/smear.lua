9670a56ed9677ca3f37e2799bdb5ff3479d35f77
Jake Ginesin <jakeginesin@gmail.com>
2025-12-18 03:13:21 -0500

    generation 550 25.11.20250714.62e0f05

--- home/programs/nvim/plugins/smear.lua
+++ home/programs/nvim/plugins/smear.lua
@@ -2,6 +2,7 @@ require('smear_cursor').setup({
   scroll_buffer_space = false,
   smear_between_buffers = false,
   smear_between_neighbor_lines = false,
+  scroll_buffer_space = true,
   stiffness = 0.5,
   trailing_stiffness = 0.5,
   matrix_pixel_threshold = 0.5,
